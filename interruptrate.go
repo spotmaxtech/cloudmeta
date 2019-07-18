56d83d22f33bf9da010acfeb1d2144291db9748a
leon <leon@mobvista.com>
2019-07-18 17:21:21 +0800

    update interrupt

--- interruptrate.go
+++ interruptrate.go
@@ -1,14 +1,13 @@
 package cloudmeta
 
 type InterruptInfo struct {
-	Name         string             `json:"name"`
-	Rate         string             `json:"rate"`
-	RateIndex    float32            `json:"rate_index"`
-	Save         float32            `json:"save"`
+	Name     string  `json:"name"`
+	Rate     string  `json:"rate"`
+	RateDesc float32 `json:"rate_desc"`
 }
 
 type InterruptAdvisor struct {
-	data   map[string]*InterruptInfo
+	data map[string]*InterruptInfo
 }
 
 func (ia *InterruptAdvisor) GetInterruptInfo(name string) *InterruptInfo {
