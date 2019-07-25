2ae0ad9b3c7f7ff7d0be1dbb51c0ef357fd56d82
leon <leon@mobvista.com>
2019-07-25 11:22:35 +0800

    update cloud meta

--- cloudmeta.go
+++ cloudmeta.go
@@ -1,11 +1,72 @@
 package cloudmeta
 
+import (
+	"fmt"
+	"github.com/spotmaxtech/gokit"
+)
+
 type DbSet struct {
 	Region    Region
 	Instance  Instance
 	Interrupt Interrupt
 	ODPrice   ODPrice
 	SpotPrice SpotPrice
+}
+
+// fetch all the meta data
+func (s *DbSet) fetch(consul *gokit.Consul) error {
+	if err := s.Region.Fetch(consul); err != nil {
+		return err
+	}
+	if err := s.Instance.Fetch(consul); err != nil {
+		return err
+	}
+	if err := s.Interrupt.Fetch(consul); err != nil {
+		return err
+	}
+	if err := s.ODPrice.Fetch(consul); err != nil {
+		return err
+	}
+	if err := s.SpotPrice.Fetch(consul); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// check the db corrupted or not
+func (s *DbSet) consistent() bool {
+	if !s.instanceConsistent() {
+		return false
+	}
+	if !s.interruptConsistent() {
+		return false
+	}
+	if !s.spotPriceConsistent() {
+		return false
+	}
+	if !s.odPriceConsistent() {
+		return false
+	}
+
+	return true
+}
+
+func (s *DbSet) instanceConsistent() bool {
+	return true
+}
+
+func (s *DbSet) interruptConsistent() bool {
+	return true
+}
+
+func (s *DbSet) spotPriceConsistent() bool {
+	return true
+}
+
+func (s *DbSet) odPriceConsistent() bool {
+	return true
+
 }
 
 func newAWSDbSet() *DbSet {
@@ -32,13 +93,15 @@ func newAliDbSet() *DbSet {
 }
 
 type MetaDb struct {
-	addr string
-	set  *DbSet
+	consul     *gokit.Consul
+	identifier CloudIdentifier
+	// TODO: add lock for db set
+	set *DbSet
 }
 
-func NewMetaDb(identifier CloudIdentifier, addr string) *MetaDb {
+func NewMetaDb(identifier CloudIdentifier, addr string) (*MetaDb, error) {
 	db := &MetaDb{
-		addr: addr,
+		consul: gokit.NewConsul(addr),
 	}
 	switch identifier {
 	case AWS:
@@ -48,46 +111,51 @@ func NewMetaDb(identifier CloudIdentifier, addr string) *MetaDb {
 	default:
 		db.set = newAWSDbSet()
 	}
-	return db
+	if err := db.set.fetch(db.consul); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
-// check the db corrupted or not
-func (m *MetaDb) Consistent() bool {
-	if !m.instanceConsistent() {
-		return false
-	}
-	if !m.interruptConsistent() {
-		return false
-	}
-	if !m.spotPriceConsistent() {
-		return false
-	}
-	if !m.odPriceConsistent() {
-		return false
-	}
-
-	return true
+// TODO: here will take much copy??
+func (m *MetaDb) Region() Region {
+	return m.set.Region
 }
 
-// update all the meta
-func (m *MetaDb) UpdateAll() error {
+func (m *MetaDb) Instance() Instance {
+	return m.set.Instance
+}
 
+func (m *MetaDb) Interrupt() Interrupt {
+	return m.set.Interrupt
+}
+
+func (m *MetaDb) ODPrice() ODPrice {
+	return m.set.ODPrice
+}
+
+func (m *MetaDb) SpotPrice() SpotPrice {
+	return m.set.SpotPrice
+}
+
+// update new meta version
+func (m *MetaDb) Update() error {
+	var set *DbSet
+	switch m.identifier {
+	case AWS:
+		set = newAWSDbSet()
+	case Ali:
+		set = newAliDbSet()
+	default:
+		set = newAWSDbSet()
+	}
+	if err := set.fetch(m.consul); err != nil {
+		return err
+	}
+	if !set.consistent() {
+		return fmt.Errorf("not consistent db set")
+	}
+
+	m.set = set
 	return nil
-}
-
-func (m *MetaDb) instanceConsistent() bool {
-	return true
-}
-
-func (m *MetaDb) interruptConsistent() bool {
-	return true
-}
-
-func (m *MetaDb) spotPriceConsistent() bool {
-	return true
-}
-
-func (m *MetaDb) odPriceConsistent() bool {
-	return true
-
 }
--- cloudmeta_test.go
+++ cloudmeta_test.go
@@ -1,7 +1,34 @@
 package cloudmeta
 
-import "testing"
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/spotmaxtech/gokit"
+	"testing"
+)
+
+func TestDbSet_Consistent(t *testing.T) {
+	Convey("test consistent", t, func() {
+		consul := gokit.NewConsul(TestConsulAddress)
+		set := newAWSDbSet()
+		err := set.fetch(consul)
+		So(err, ShouldBeNil)
+		So(set.consistent(), ShouldBeTrue)
+	})
+}
 
 func TestMetaDb(t *testing.T) {
+	Convey("test meta db", t, func() {
+		meta, err := NewMetaDb(AWS, TestConsulAddress)
+		So(err, ShouldBeNil)
+		meta.Interrupt()
+		So(meta.Region().GetRegionInfo("us-east-1").Name, ShouldEqual, "us-east-1")
+	})
+}
 
+func TestMetaDb_Update(t *testing.T) {
+	Convey("test bla bla", t, func() {
+		meta, err := NewMetaDb(AWS, TestConsulAddress)
+		So(err, ShouldBeNil)
+		So(meta.Update(), ShouldBeNil)
+	})
 }
