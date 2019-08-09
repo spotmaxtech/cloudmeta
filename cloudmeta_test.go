86f0ac237cfc4c8c6dd00a454d9b19dd766043f0
Liuzoxan <liuzoxan@gmail.com>
2019-08-09 11:24:45 +0800

    add more metadb test

--- cloudmeta_test.go
+++ cloudmeta_test.go
@@ -26,7 +26,7 @@ func TestMetaDb(t *testing.T) {
 }
 
 func TestMetaDb_Update(t *testing.T) {
-	Convey("test bla bla", t, func() {
+	Convey("test update", t, func() {
 		meta, err := NewMetaDb(AWS, TestConsulAddress)
 		So(err, ShouldBeNil)
 		So(meta.Update(), ShouldBeNil)
@@ -34,4 +34,19 @@ func TestMetaDb_Update(t *testing.T) {
 		So(meta.Update(), ShouldBeNil)
 		So(meta.Region().GetRegionInfo("us-east-1").Name, ShouldEqual, "us-east-1")
 	})
+}
+
+func TestMetaDb_DefaultDb(t *testing.T) {
+	Convey("test instance", t, func() {
+		meta := DefaultAWSMetaDb()
+		So(meta.Region().GetRegionInfo("us-east-1").Name, ShouldEqual, "us-east-1")
+		t.Log(gokit.Prettify(meta.Region().GetRegionInfo("us-east-1")))
+		t.Log(gokit.Prettify(meta.Interrupt().GetInterruptInfo("us-east-1", "c4.2xlarge")))
+		t.Log(gokit.Prettify(meta.ODPrice().GetPrice("us-east-1", "c4.2xlarge")))
+		t.Log(gokit.Prettify(meta.SpotPrice().GetPrice("us-east-1", "c4.2xlarge")))
+
+		t.Log(gokit.Prettify(meta.Region().List()))
+		// t.Log(gokit.Prettify(meta.Instance().List("us-west-2")))
+	})
+
 }
