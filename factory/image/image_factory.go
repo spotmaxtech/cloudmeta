45a399bd81fba89e191c917520f0938287e33e96
zhaoying <zying1023@gmail.com>
2020-05-19 15:06:14 +0800

    update aws image meta

--- factory/image/image_factory.go
+++ factory/image/image_factory.go
@@ -28,6 +28,41 @@ type ImageMap struct {
 	data map[string]map[string]map[string]*ec2.Image
 }
 
+func (iu *ImageUtil) FetchImageList (accountId []*string, owner []*string, name string, num int) *[]*ec2.Image{
+	var imageList []*ec2.Image
+	input := &ec2.DescribeImagesInput{
+		ExecutableUsers: accountId,
+		Filters:         []*ec2.Filter{
+			{
+				Name:   aws.String("name"),
+				Values: aws.StringSlice([]string{name}),
+			},
+			{
+				Name:	aws.String("state"),
+				Values: aws.StringSlice([]string{"available"}),
+			},
+		},
+		Owners:          owner,
+	}
+	result, err := iu.Conn.EC2.DescribeImages(input)
+	if err != nil {
+		return nil
+	}
+	if result != nil {
+		if len(result.Images) > 0{
+			sort.Slice(result.Images, func(i, j int) bool {
+				var timeFormat = "2006-01-02T15:04:05.000Z"
+				timestamp_i, _ := time.Parse(timeFormat, *result.Images[i].CreationDate)
+				timestamp_j, _ := time.Parse(timeFormat, *result.Images[j].CreationDate)
+				return timestamp_i.Unix() > timestamp_j.Unix()
+			})
+			imageList = result.Images[0:num]
+			return &imageList
+		}
+	}
+	return &imageList
+}
+
 func (iu *ImageUtil) FetchImage (accountId []*string, ownerId []*string, name string) *ec2.Image{
 	input := &ec2.DescribeImagesInput{
 		ExecutableUsers: accountId,
@@ -67,6 +102,48 @@ func (iu *ImageUtil) FetchImage (accountId []*string, ownerId []*string, name st
 		}
 	}
 	return nil
+}
+
+func getImageMapList(accountId []*string, ownerId []*string, len int) *ImageMap{
+	imageName := []string{"amzn2-ami*","amzn-ami*","ubuntu*","RHEL*","suse*"}
+	imageMap := ImageMap{
+		data: make(map[string]map[string]map[string]*ec2.Image),
+	}
+	consul := gokit.NewConsul(ConsulAddr)
+	metaRegion := cloudmeta.NewCommonRegion(RegionKey)
+	if err := metaRegion.Fetch(consul); err != nil {
+		panic(err)
+	}
+
+	for _, region := range metaRegion.List() {
+		fmt.Println(region.Name)
+		util := ImageUtil{Conn:connections.New(region.Name)}
+		imageMap.data[region.Name] = make(map[string]map[string]*ec2.Image)
+		var imageType = []string{"Linux","SUSE","Red Hat","Windows"}
+		for _, v := range imageType {
+			imageMap.data[region.Name][v] = make(map[string]*ec2.Image)
+		}
+
+		for _, v := range imageName {
+			imageList := util.FetchImageList(accountId, ownerId, v, len)
+			if imageList != nil {
+				for _, image := range *imageList {
+					switch  {
+					case strings.Contains(*image.Name, "amzn") || strings.Contains(*image.Name, "ubuntu"):
+						imageMap.data[region.Name]["Linux"][*image.Name] = image
+					case strings.Contains(*image.Name, "suse"):
+						imageMap.data[region.Name]["SUSE"][*image.Name] = image
+					case strings.Contains(*image.Name, "RHEL"):
+						imageMap.data[region.Name]["Red Hat"][*image.Name] = image
+					default:
+						imageMap.data[region.Name]["Linux"][*image.Name] = image
+					}
+				}
+			}
+		}
+	}
+
+	return &imageMap
 }
 
 func getImageMap(accountId []*string, ownerId []*string) *ImageMap {
@@ -118,7 +195,9 @@ func main() {
 	//accountId = append(accountId, &id)
 	//ownerId = append(ownerId, &id, &awsid)
 
-	imageMap := getImageMap(nil,nil)
+	//imageMap := getImageMap(nil,nil)
+
+	imageMap := getImageMapList(nil, nil, 5)
 
 	bytes, err := json.MarshalIndent(imageMap.data, "", "    ")
 	if err != nil {
