8a7e2ad72d33d5ce3bf79edf3986e2ac6dfbb6c1
zhaoying <zying1023@gmail.com>
2021-01-06 17:01:36 +0800

    v1 update init method

--- defaultmeta.go
+++ defaultmeta.go
@@ -6,16 +6,26 @@ var once sync.Once
 var awsMeta *MetaDb
 var aliMeta *ALiMetaDB
 
+var consulAddr string
+
 func DefaultAWSMetaDb() *MetaDb {
 	once.Do(func() {
-		awsMeta, _ = NewMetaDBAWS(TestConsulAddress)
+		awsMeta, _ = NewMetaDBAWS(consulAddr)
 	})
 	return awsMeta
 }
 
 func DefaultAliMetaDb() *ALiMetaDB {
 	once.Do(func() {
-		aliMeta, _ = NewMetaDBALi(TestConsulAddress)
+		aliMeta, _ = NewMetaDBALi(consulAddr)
 	})
 	return aliMeta
+}
+
+func Initialize(consul string) {
+	consulAddr = consul
+}
+
+func init() {
+	consulAddr = TestConsulAddress
 }
